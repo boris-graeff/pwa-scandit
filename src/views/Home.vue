71bead8c802fbea29587c639e1bbcebc3975a4de
Boris <boris.graeff@gmail.com>
2018-09-26 15:55:37 +0200

    Set up scandit barcode reader

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    HOME
+    <div class="scandit" ref="scandit"></div>
+
+    {{ scan }}
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import { configure, BarcodePicker, ScanSettings } from 'scandit-sdk';
+import config from '../../config.json';
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      scan: {},
+    };
+  },
+  async mounted() {
+
+    const engineLocation = '/build';
+    await configure(config.scandit.apiKey, { engineLocation });
+
+    this.barcodePicker = await BarcodePicker.create(this.$refs.scandit, {
+      playSoundOnScan: false,
+      vibrateOnScan: false,
+      preloadEngineLibrary: true,
+      preloadCameras: true,
+    });
+
+    const scanSettings = new ScanSettings({
+      enabledSymbologies: ['qr', 'code128'],
+      codeDuplicateFilter: 3000,
+    });
+
+    this.barcodePicker.applyScanSettings(scanSettings);
+
+    this.barcodePicker.onScan((scan) => {
+      this.scan = scan.barcodes;
+    });
+  },
+  beforeDestroy() {
+    this.barcodePicker.destroy();
+  },
+  methods: {
+    onScan(scan) {
+
+    },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+  .scandit {
+    height: 50vh;
+  }
+</style>
